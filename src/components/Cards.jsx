12e8b62046b4acff4f1175501379ab682726082b
Jetha Ram <jetha3317@gmail.com>
2025-09-01 01:32:42 +0530

    Quick React Practice Task | Docs Mini App | React | Framer Motion

---
+++ src/components/Cards.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { FaRegFileAlt } from "react-icons/fa";
+import { LuDownload } from 'react-icons/lu';
+import { IoClose } from 'react-icons/io5';
+import { motion } from "framer-motion";
+
+
+function Cards({data, reference}){
+  return (
+		<>
+			<motion.div
+				drag
+				dragConstraints={reference}
+				whileDrag={{ scale: 1.2 }}
+				dragElastic={0.2}
+				className="relative flex-shrink-0 px-8 py-8 overflow-hidden shadow-lg text-zinc-700 h-60 w-52 bg-zinc-100 rounded-3xl"
+			>
+				<FaRegFileAlt />
+				<p className="mt-5 text-sm font-semibold leading-tight">{data.desc}</p>
+				{/* footer */}
+				<div className="absolute bottom-0 left-0 w-full ">
+					<div className="flex items-center justify-between px-8 py-2 mb-3">
+						<h5>{data.fileSize}</h5>
+						<span className="flex items-center justify-center text-white rounded-full h-7 w-7 bg-zinc-800">
+							{data.close ? <IoClose /> : <LuDownload className="w-4 h-4 " />}
+						</span>
+					</div>
+					{data.tag.isOpen && (
+						<div
+							className={`flex items-center justify-center w-full py-3 ${
+								data.tag.tagColor === "blue" ? "bg-blue-600" : "bg-green-600"
+							} `}
+						>
+							<h3 className="text-sm font-semibold">{data.tag.tagTitle}</h3>
+						</div>
+					)}
+				</div>
+			</motion.div>
+		</>
+	);
+};
+export default Cards
---
+++ src/components/Foreground.jsx
@@ -0,0 +1,40 @@
+import React, { useRef } from 'react'
+import Cards from './Cards'
+
+function Foreground() {
+
+    const ref = useRef(null);
+    const data = [
+		{
+			desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+			fileSize: ".9mb",
+			close: true,
+			tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" },
+		},
+		{
+			desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+			fileSize: ".2mb",
+			close: true,
+			tag: { isOpen: true, tagTitle: "Download Now", tagColor: "blue" },
+		},
+		{
+			desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+			fileSize: ".7mb",
+			close: false,
+			tag: { isOpen: false, tagTitle: "Download Now", tagColor: "green" },
+		},
+	];
+  return (
+    <>
+        <div
+				ref={ref}
+				className=" fixed z-[3] left-0 top-0 w-full h-full flex flex-wrap gap-5 ">
+				{ data.map((item, index) => (
+					<Cards data={item} reference={ref} />
+				))}
+			</div>
+    </>
+      )
+}
+
+export default Foreground
